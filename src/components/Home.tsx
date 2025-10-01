049554f999266a4d6e0335e7c6ec50f91e2d3424
Vamsi Aravala <saivamsiaravala9333@gmail.com>
2025-10-01 19:24:46 +0530

    add local storage

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -3,6 +3,7 @@ import Tasks from "./Tasks";
 import { useAppSelector } from "../hooks";
 import { RiAddCircleFill } from "react-icons/ri";
 import { useNavigate } from "react-router-dom";
+import { useEffect } from "react";
 
 export type Task = {
   id: number;
@@ -14,7 +15,50 @@ export type Task = {
 
 const Home = () => {
   const { tasks } = useAppSelector((store) => store.tasks);
-
+  useEffect(() => {
+    localStorage.setItem(
+      "Tasks",
+      JSON.stringify({
+        tasks: [
+          {
+            id: 1,
+            status: "In Progress",
+            title: "Lorem Ipsum 1",
+            description:
+              "Lorem ipsum dolor sit amet consectetur adipisicing elit.",
+            time: "Sep 29 2025",
+          },
+          {
+            id: 2,
+            status: "In Progress",
+            title: "Lorem Ipsum 2",
+            description:
+              "Lorem ipsum dolor sit amet consectetur adipisicing elit.",
+            time: "Sep 29 2025",
+          },
+          {
+            id: 3,
+            status: "Pending",
+            title: "Lorem Ipsum 3",
+            description:
+              "Lorem ipsum dolor sit amet consectetur adipisicing elit.",
+            time: "Sep 29 2025",
+          },
+          {
+            id: 4,
+            status: "Completed",
+            title: "Lorem Ipsum 4",
+            description:
+              "Lorem ipsum dolor sit amet consectetur adipisicing elit.",
+            time: "Sep 29 2025",
+          },
+        ],
+      })
+    );
+    return () => {
+      localStorage.clear();
+    };
+  }, []);
   const navigate = useNavigate();
 
   return (
